3119c32a9d4fc110075b84028ce216dca670d4f2
AosenC <82334948+Aosen-C@users.noreply.github.com>
2026-09-16 15:00:24 -0700

    Add files via upload

---
+++ ConvertTime.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ConvertTime {
+	public static void main(String[] args) {
+		
+		// Creates the scanner
+		Scanner in = new Scanner(System.in);
+		
+		// Asks user for the number of seconds
+		System.out.print("Enter the number of seconds: ");
+		int Seconds = in.nextInt();
+		
+		// Creates final constants
+		final int SECONDS_TO_HOURS = (60*60);
+		final int SECONDS_TO_MINUTES = 60;
+		
+		// Calculates the hours and minutes
+		
+		
+		}
+	}
