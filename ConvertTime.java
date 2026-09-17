17b3db6bf8450b9e45a31564a1963b7a489a0c72
AosenC <82334948+Aosen-C@users.noreply.github.com>
2026-09-16 22:26:56 -0700

    Add files via upload

--- ConvertTime.java
+++ ConvertTime.java
@@ -11,13 +11,13 @@ public class ConvertTime {
 		int Seconds = in.nextInt();
 		
 		// Creates final constants
-		final int SECONDS_AND_HOURS = 3600;
-		final int SECONDS_AND_MINUTES = 60;
+		final int SECONDS_TO_HOURS = 3600;
+		final int SECONDS_TO_MINUTES = 60;
 		
 		// Calculates the hours and minutes and seconds
-		int Hour = Seconds / SECONDS_AND_HOURS;
-		int Minutes = (Seconds % SECONDS_AND_HOURS) / SECONDS_AND_MINUTES;
-		int Rem_Seconds = (Seconds % SECONDS_AND_HOURS) % SECONDS_AND_MINUTES;
+		int Hour = Seconds / SECONDS_TO_HOURS;
+		int Minutes = (Seconds % SECONDS_TO_HOURS) / SECONDS_TO_MINUTES;
+		int Rem_Seconds = (Seconds % SECONDS_TO_HOURS) % SECONDS_TO_MINUTES;
 		
 		// Prints the results
 		System.out.printf("%d seconds = %d hours, %d minutes, and %d seconds", Seconds, Hour, Minutes, Rem_Seconds);
